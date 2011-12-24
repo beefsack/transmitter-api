404e4e3ef3b70db4ae8b21913805770852aa9ea9
Michael Alexander <beefsack@gmail.com>
2011-12-24 23:59:03 +1100

    Added templates.

--- transmitter-api.rb
+++ transmitter-api.rb
@@ -4,7 +4,17 @@ require 'json'
 require 'transmitter-data'
 
 disable :protection
+set :haml, :format => :html5, :layout => true
 
 get '/' do
-  'egg'
+  haml :index
+end
+
+get '/hunt' do
+  @transmitter_data =
+    TransmitterHunter::TransmitterData.new
+  @transmitter_data.transmitters.each do |t|
+    puts YAML::dump(t)
+  end
+  haml :hunt
 end
